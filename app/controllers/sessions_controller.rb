bcf1fa65089306b9d90c7c9246d66f4ff28ddf95
Bryce Boe <bbzbryce@gmail.com>
2016-11-21 15:33:03 -0800

    [WIP] Add session create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,21 @@
 class SessionsController < ApplicationController
+  def create
+    @session_form = SessionForm.new(session_params)
+    if @session_form.valid?
+      user = User.find_by(email: @session_form)
+      redirect_to new_session_path, flash: { success: 'Logged in' }
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def new
     @session_form = SessionForm.new
+  end
+
+  private
+
+  def session_params
+    params.require(:session_form).permit(:email, :password)
   end
 end
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,10 +1,23 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
+  include TestParams
+
   test 'new session' do
     get new_session_path
     assert_response :ok
     assert_select 'h1', 'Log In'
-    assert_select "form[action='#{new_session_path}']"
+    assert_select "form[action='#{sessions_path}']"
+  end
+
+  test 'create valid session' do
+    post sessions_path(session_form: session_form_params)
+    assert_redirected_to new_session_path
+    assert_equal 'Logged in', flash[:success]
+  end
+
+  test 'create invalid session' do
+    post sessions_path(session_form: session_form_params(email: ''))
+    assert_response :unprocessable_entity
   end
 end
